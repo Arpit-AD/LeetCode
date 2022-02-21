335bad948e90c237c38503155a6768a2b967cc3f
Arpit-AD <78785430+Arpit-AD@users.noreply.github.com>
2022-02-21 18:46:38 +0530

    Time: 24 ms (60.88%), Space: 19.7 MB (52.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int> k;
-        int n;
-        for(auto z: nums)
-        {
-            k[z]++;
-            if(k[z] > nums.size()/2)
-                n = z;
-        }
-        
-        return n;
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
         
     }
 };
